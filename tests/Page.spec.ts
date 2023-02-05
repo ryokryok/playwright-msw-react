2912c80afdaee1e6ab3686937ad1731b1a357045
mr_ozin <mrozin.dev@gmail.com>
2023-02-05 21:18:43 +0900

    rewrite e2e test

--- tests/Page.spec.ts
+++ tests/Page.spec.ts
@@ -2,12 +2,24 @@ import { test, expect } from "@playwright/test";
 
 test("Capture screenshots", async ({ page, browserName }) => {
   await page.goto("/");
+  await page.waitForSelector("form");
   await page.screenshot({ path: `screenshot/${browserName}/screenshot.png` });
 });
 
-test("Page title", async ({ page }) => {
+test("success to form send", async ({ page }) => {
   await page.goto("/");
-  expect(await page.title()).toBe("Index");
-  await page.goto("/contact/");
-  expect(await page.title()).toBe("Contact");
+  await page.waitForSelector("form");
+  const input = page.locator("input[type=email]");
+  await input.fill("user@example.com");
+  await page.getByRole("button").click();
+  await expect(page.getByText("success to contact")).toBeVisible();
+});
+
+test("failed to form send", async ({ page }) => {
+  await page.goto("/");
+  await page.waitForSelector("form");
+  const input = page.locator("input[type=email]");
+  await input.fill("");
+  await page.getByRole("button").click();
+  await expect(page.getByText("error, please resend")).toBeVisible();
 });
